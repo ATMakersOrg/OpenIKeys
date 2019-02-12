c762b5bb3b25e19f713e0a44c1c0a54bfe724c65
Matt Gonzalez <matt@echoaudio.com>
2019-02-11 18:32:10 -0800

    New INF file (generated from INX file) Removed unused files Added script for generating x86 signed driver cabinet

--- original/IntelliKeys/WindowsOld/Win/Loading Driver/prepare_w10_x64.cmd
+++ original/IntelliKeys/WindowsOld/Win/Loading Driver/prepare_w10_x64.cmd
@@ -4,7 +4,7 @@
 mkdir w10_x64
 copy x64\Release\ikusb.sys w10_x64
 copy x64\Release\ikusb.pdb w10_x64
-copy ikusb.inf w10_x64
+copy x64\Release\ikusb.inf w10_x64
 @rem
 @rem Create the cab file
 @rem
@@ -13,3 +13,4 @@ makecab /f w10_x64.ddf
 @rem Sign the cab file
 @rem 
 SignTool sign /ac "GlobalSign Root CA.crt" /s MY /n "Echo Digital Audio Corporation" /fd sha256 /tr http://timestamp.globalsign.com/scripts/timestamp.dll  /td sha256 /v "disk1\ikusb_w10_x64.cab"
+@pause
